afd79391b2b5e636d9b4f370f233b05189468a85
fedark <fedark@github.com>
2022-10-27 13:14:44 +0300

    Clean up authentication related code.

--- LocalLangUI/Startup.cs
+++ LocalLangUI/Startup.cs
@@ -1,42 +1,26 @@
 using LocalLangLibrary.DataAccess;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Identity.Web;
 
 namespace LocalLangUI
 {
-    public class Startup
+	public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddMemoryCache();
-
-            //services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-            //    .AddMicrosoftIdentityWebApp(Configuration, configSectionName: "AzureAdB2C");
-
-            //services.AddAuthorization(options =>
-            //{
-            //    options.AddPolicy("Admin", policy =>
-            //    {
-            //        policy.RequireClaim("jobTitle", "Admin");
-            //    });
-            //});
 
             services.AddSingleton<IDbConnection, DbConnection>();
             services.AddSingleton<ICategoryData, MongoCategoryData>();
@@ -45,7 +29,6 @@ namespace LocalLangUI
             services.AddSingleton<IExpressionData, MongoExpressionData>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -55,7 +38,6 @@ namespace LocalLangUI
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -63,17 +45,6 @@ namespace LocalLangUI
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            //app.UseAuthentication();
-            //app.UseAuthorization();
-
-            //app.UseRewriter(new RewriteOptions().Add(context =>
-            //{
-            //    if (context.HttpContext.Request.Path == "/MicrosoftIdentity/Account/SignedOut")
-            //    {
-            //        context.HttpContext.Response.Redirect("/");
-            //    }
-            //}));
 
             app.UseEndpoints(endpoints =>
             {

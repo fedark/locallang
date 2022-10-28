9e13f8f3652ba7c40d0c0862c3e62ce85eee29a2
fedark <fedark@github.com>
2022-10-28 23:01:41 +0300

    Localized index page.

--- LocalLangUI/Localization/Localization.cs
+++ LocalLangUI/Localization/Localization.cs
@@ -5,8 +5,18 @@
 		public string PageHeading { get; } = "Славечкi";
 		public string SuggestButton { get; } = "Предложить";
 		public string ExpressionPlural { get; } = "Выражений";
+
 		public string SortAlphabet { get; } = "Абв";
 		public string SortNew { get; } = "Новые";
 		public string SortPopular { get; } = "Популярные";
+
+		public string SearchPlaceholder { get; } = "Поиск";
+
+		public string LikeTopText { get; } = "Лайкнуть";
+		public string LikeBottomText { get; } = "Лайк";
+		public string LikeBottomTextPlural { get; } = "Лайков";
+
+		public string CategoryHeading { get; } = "Категория";
+		public string CategoryAll{ get; } = "Все";
 	}
 }

cb23a220f6d775fcb77fad2c82b0645a48b0d302
fedark <fedark@github.com>
2022-10-29 11:55:10 +0300

    Localized validation messages.

--- LocalLangUI/Models/CreateExpressionModel.cs
+++ LocalLangUI/Models/CreateExpressionModel.cs
@@ -1,19 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using LocalLangUI.Resources;
 
 namespace LocalLangUI.Models
 {
 	public class CreateExpressionModel
 	{
-		[Required]
-		[MaxLength(75)]
+		[Required(ErrorMessageResourceName = nameof(ErrorMessageResource.ExpressionRequiredError), ErrorMessageResourceType = typeof(ErrorMessageResource))]
+		[MaxLength(75, ErrorMessageResourceName = nameof(ErrorMessageResource.ExpressionInvalidError), ErrorMessageResourceType = typeof(ErrorMessageResource))]
 		public string? Word { get; set; }
 
-		[Required]
-		[MaxLength(500)]
+		[Required(ErrorMessageResourceName = nameof(ErrorMessageResource.TranslationRequiredError), ErrorMessageResourceType = typeof(ErrorMessageResource))]
+		[MaxLength(500, ErrorMessageResourceName = nameof(ErrorMessageResource.TranslationInvalidError), ErrorMessageResourceType = typeof(ErrorMessageResource))]
 		public string? Translation { get; set; }
 
-		[Required]
-		[MinLength(1)]
+		[Required(ErrorMessageResourceName = nameof(ErrorMessageResource.CategoryRequiredError), ErrorMessageResourceType = typeof(ErrorMessageResource))]
+		[MinLength(1, ErrorMessageResourceName = nameof(ErrorMessageResource.CategoryInvalidError), ErrorMessageResourceType = typeof(ErrorMessageResource))]
 		[Display(Name = "Category")]
 		public string? Category { get; set; }
 	}

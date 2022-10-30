ad7fefc5c53426a6776feba4292c41692c5469ba
fedark <fedark@github.com>
2022-10-30 15:09:17 +0300

    Cleaned up error page.

--- LocalLangUI/Pages/Error.cshtml.cs
+++ LocalLangUI/Pages/Error.cshtml.cs
@@ -1,27 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LocalLangUI.Pages
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-        private readonly ILogger<ErrorModel> _logger;
+        private readonly ILogger<ErrorModel> logger_;
 
         public ErrorModel(ILogger<ErrorModel> logger)
         {
-            _logger = logger;
+            logger_ = logger;
         }
 
         public void OnGet()

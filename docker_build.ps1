f2a5b51840dcdb6e8a3b5c6f782734576f0b23eb
fedark <fedark@github.com>
2023-02-21 20:49:46 +0300

    Hide connection string.

--- docker_build.ps1
+++ docker_build.ps1
@@ -1,3 +1,3 @@
-docker stop locallangui
+docker stop langweb
 docker build -t fedark/locallangweb -f Dockerfile .
-docker run -td --rm --name locallangui -p 7666:7666 fedark/locallangweb
+docker run -td --rm --name langweb -p 7666:7666 fedark/locallangweb
